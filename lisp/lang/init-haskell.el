2199e683ce572222d2501f44b6266f1ca8cea58f
Linyu Yang <yly1228@foxmail.com>
2025-11-01 16:48:26 +0800

    updated: haskell

--- lisp/lang/init-haskell.el
+++ lisp/lang/init-haskell.el
@@ -8,11 +8,11 @@
 (use-package haskell-mode
   :defer t
   :after (eglot company)
-  :config
+  :init
   (setq eglot-workspace-configuration
         (plist-put eglot-workspace-configuration
                    :haskell
-                   (:formattingProvider . "fourmolu")))
+                   (:formattingProvider "fourmolu")))
   :bind
   (:map
    haskell-mode-map                     ;haskell files
